5bf1df6cc29bb4f52267551390c7c6e0f6fc5a24
Subhash Kumar Singh <subhashyadav2015jack@gmail.com>
2026-06-30 20:22:10 +0530

    list_method

--- Conditional Statements/list.py
+++ Conditional Statements/list.py
@@ -1,18 +1,34 @@
-l = [4,5,6,"Subhash",True,"99"]
-print(l)
-print(type(l))
-print(l[0])
-print(l[1])
-print(l[2])
-print(l[3])
-print(l[4])
-print(l[5])
+# marks = [4,5,6,"Subhash",True,"99",55,44,98,45,10,18]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+# print(marks[3])
+# print(marks[4])
+# print(marks[5])
 
-print(l[-3])
-print(l[-4])
+# print(marks[-3])
+# print(marks[-4])
 
 
 # if "ash" in "Subhash":
 #     print(True)
-# else:
-#     print(False)
+# emarksse:
+#     print(Famarksse)
+
+# marks = [4,5,6,"Subhash",True,"99",55,44,98,45,10,18]
+
+# print(marks)
+# print(marks[1:9])
+# print(marks[1:9:3])
+# print(marks[:4])
+
+
+
+
+lst= [i*i for i in range(10)]
+print(lst)
+
+lst1 = [i*i for i in range(10) if i%2==0 ]
+print(lst1)
