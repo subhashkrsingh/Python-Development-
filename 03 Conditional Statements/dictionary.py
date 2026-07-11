3f66a7c722cfb748d2bd80046c1b6b53968b158a
Subhash Kumar Singh <subhashyadav2015jack@gmail.com>
2026-07-11 11:19:51 +0530

    Dictionary,set

---
+++ 03 Conditional Statements/dictionary.py
@@ -0,0 +1,22 @@
+# dic ={'name' : 'Subhash Kumar Singh',
+#      'age': 23,
+#      'roll': 1
+# }
+
+# print(dic['name'])
+
+# info ={
+#     'name':'Subhash', 'age':'23', 'eligible': True
+# }
+# print(info)
+
+# for keys in info:
+#     print(info[keys])
+
+emp1 ={121:68, 123:54, 132:64, 140:75}
+emp2 = {125:59, 128:67, 145:86}
+
+# emp1.update(emp2)
+# emp1.clear ()
+emp1.popitem()
+print(emp1)
--- 03 Conditional Statements/set.py
+++ 03 Conditional Statements/set.py
@@ -1,10 +1,36 @@
-set = {2,5,7,5,6,2,4}
-print(set)
+# set = {2,5,7,5,6,2,4}
+# print(set)
 
-item = {"ram" ,"Subhash",15, 21,7}
-print(item)
-print(type(item))
+# item = {"ram" ,"Subhash",15, 21,7}
+# print(item)
+# print(type(item))
 
-for value in item:
-    print(value)
+# for value in item:
+#     print(value)
     
+
+s1 = {1,3,4,5,7}
+s2 = {0,2,4,6,8,}
+
+# # print(s1.union(s2))
+# # print(s1.intersection(s2))
+# s1.update(s2) print(s1,s2)
+
+
+# print(s1.isdisjoint(s2))
+
+# print(s1.issuperset(s2))
+# print(s1.issubset(s2))
+
+# s1.add(9)
+# print(s1)
+
+
+# s1.discard(4)
+# print(s1)
+
+i= s1.pop()
+print(s1)
+del i
+print(i)
+
