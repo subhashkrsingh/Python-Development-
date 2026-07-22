89c73e31c6a160bc69dee8b26ad7ae7b2b0f2801
Subhash Kumar Singh <subhashyadav2015jack@gmail.com>
2026-07-22 17:38:55 +0530

    import.py,subhash.py

---
+++ Important_Topics.py/enumerate.py
@@ -0,0 +1,15 @@
+marks = [14,58,65,33,27,81,64,6]
+
+
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if(index == 3):
+#         print("Subhash is Awesome!")
+#     index += 1
+
+
+for index, mark in enumerate(marks) :
+    print(mark)
+    if(index == 5):
+        print("Subhash is Awesome!")
---
+++ Important_Topics.py/short_hand-if_else.py
@@ -0,0 +1,7 @@
+a = 5245
+b = 5468
+
+print("A") if a<b else print("=") if a == b else print("B")
+
+c = 9 if  a>b else 0
+print(c) 
